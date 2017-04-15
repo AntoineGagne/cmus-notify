9c61d38932d6f8523fa3c162c6a43b801e834537
Antoine Gagné <gagnantoine@gmail.com>
2017-04-14 21:04:54 -0400

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-#!/usr/bin/env python
+#! /usr/bin/env python3
 
 import os
 from setuptools import setup
 
 
-def get_long_description(file_name):
+def get_long_description(file_name: str) -> str:
     """Gets the long description from the specified file's name.
+
     :param file_name: The file's name
     :type file_name: str
     :return: The content of the file
@@ -16,26 +17,19 @@ def get_long_description(file_name):
 
 if __name__ == '__main__':
     setup(
-        name='design',
-        version='1.0.0.1',
-        description='A package for the course *Design III*',
-        author=('Antoine Gagne'
-                'Nadia Chicoine <nadia.chicoine.2@ulaval.ca>,'
-                'Alexandre Tremblay <alexandre.tremblay.18@ulaval.ca>,'
-                'Imen Daagi <imen.daagi.1@ulaval.ca>'),
-        keywords='robotics numeric vision socket programming',
+        name='cmus-notify',
+        version='0.0.0.1',
+        description='A package for displaying Cmus current status in notifications',
+        author='Antoine Gagne'
+        keywords='utilities application cli hook',
         author_email='antoine.gagne.2@ulaval.ca',
-        url='https://github.com/AntoineGagne/design-3-glo',
-        packages=['design'],
-        data_files=[('config', ['config/camera_optimized_values.json'])],
+        url='https://github.com/AntoineGagne/cmus-notify',
+        packages=['cmus_notify'],
+        data_files=[],
         include_package_data=True,
         long_description=get_long_description('README.md'),
         setup_requires=['pytest-runner', 'flake8'],
         tests_require=['pytest'],
         test_suite='tests',
-        scripts=['scripts/extract_data_samples.py',
-                 'scripts/download_datasets.py',
-                 'scripts/world_image_items_identifier_ui.py',
-                 'scripts/calibrate.py',
-                 'scripts/vertices_identifier.py']
+        scripts=[]
     )
