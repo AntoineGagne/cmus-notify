a26a9a7ba2697d589add52d328cefcf770b8bdbc
Antoine Gagné <gagnantoine@gmail.com>
2017-04-15 16:00:58 -0400

    Add an argument parser

--- cmus_notify/cmus_notify.py
+++ cmus_notify/cmus_notify.py
@@ -20,9 +20,12 @@
 
 """Contains the main code of the package."""
 
-import sys
+from argparse import RawDescriptionHelpFormatter, ArgumentParser
 
-from .constants import DEFAULT_APPLICATION_NAME, ICONS_BY_STATUS
+from .constants import (DEFAULT_APPLICATION_NAME,
+                        DEFAULT_MESSAGE_BODY,
+                        DEFAULT_MESSAGE_TITLE,
+                        ICONS_BY_STATUS)
 from .formatters import format_notification_message
 from .notifications import Notifier
 from .parsers import parse_status_information
@@ -30,11 +33,68 @@ from .parsers import parse_status_information
 
 def main():
     """Main entry point of the package."""
-    information = parse_status_information(sys.argv[1])
-    title, text = format_notification_message(information)
-    notifier = Notifier(DEFAULT_APPLICATION_NAME)
+    arguments = _parse_arguments()
+    information = parse_status_information(arguments.parse)
+    title, text = format_notification_message(information,
+                                              title=arguments.title,
+                                              body=arguments.body)
+    notifier = Notifier(arguments.application_name)
     notifier.send_notification(
         title,
         text,
-        icon_path=ICONS_BY_STATUS.get(information.status, '')
+        icon_path=ICONS_BY_STATUS.get(information.status.lower(), '')
     )
+
+
+def _parse_arguments():
+    """Create an :class:`argparse.ArgumentParser` and parse the command-line arguments."""
+
+    parser = ArgumentParser(formatter_class=RawDescriptionHelpFormatter,
+                            prog='cmus_notify',
+                            description='Display a notification about Cmus\'s '
+                                        'current status',
+                            epilog='Format String Parameters\n'
+                                   '========================\n\n'
+                                   'The available arguments to the format '
+                                   'strings are the following:\n\n'
+                                   '  - album: The song\'s album\n'
+                                   '  - artist: The song\'s artist\n'
+                                   '  - date: The song\'s release date\n'
+                                   '  - discnumber: The song\'s disc\'s number\n'
+                                   '  - duration: The song\'s duration\n'
+                                   '  - file: The song\'s file\'s path\n'
+                                   '  - status: Cmus current status\n'
+                                   '  - title: The song\'s title\n'
+                                   '  - tracknumber: The song\'s track number')
+    parser.add_argument('parse',
+                        type=str,
+                        metavar='INFORMATION',
+                        help='Parse the given information')
+    parser.add_argument('-a',
+                        '--application_name',
+                        default=DEFAULT_APPLICATION_NAME,
+                        type=str,
+                        help='The name of the application')
+    parser.add_argument('-b',
+                        '--body',
+                        required=False,
+                        metavar='BODY_FORMAT_STRING',
+                        default=DEFAULT_MESSAGE_BODY,
+                        type=str,
+                        help='A format string that can be specified to tell the'
+                             ' software how to format the body. The syntax is '
+                             'the same as Python\'s. The available options are '
+                             'specified at the end of this help message. (i.e. '
+                             '\'Artist: {artist}\')')
+    parser.add_argument('-t',
+                        '--title',
+                        required=False,
+                        metavar='TITLE_FORMAT_STRING',
+                        default=DEFAULT_MESSAGE_TITLE,
+                        type=str,
+                        help='A format string that can be specified to tell the'
+                             ' software how to format the title. The syntax is '
+                             'the same as Python\'s. The available options are '
+                             'specified at the end of this help message. (i.e. '
+                             '\'Now playing: {title}\')')
+    return parser.parse_args()
